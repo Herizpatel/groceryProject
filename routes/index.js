45a3de4c4ed3fa9adf9f9f63c538f328210997fe
Heriz <herrypatel3369@gmail.com>
2020-12-10 13:36:31 -0500

    Add grocery functionality is done

--- routes/index.js
+++ routes/index.js
@@ -21,16 +21,23 @@ router.get('/groceries', async (req, res) => {
 
 })
 
-// // GET Post Submission Form
-// router.get('/post', (req, res) => {
-//   res.render('new-post', {title: "Submit Post"})
-// })
-//
-// // POST New Post
-// router.post('/post', (req, res) => {
-//   console.log(req.body)
-//   res.send({content: "All good!"})
-// })
+// GET Post Submission Form
+router.get('/grocery', (req, res) => {
+  res.render('newGrocery', {title: "Add Grocery", msg: ""})
+})
+
+// POST New Grocery
+router.post('/grocery', async (req, res) => {
+  let groceryName = req.body.groceryName
+  let groceryCost = req.body.groceryCost
+  let added = await groceries_model.add_grocery(groceryName, groceryCost)
+
+  if(added) {
+    res.redirect(`/groceries`) // If user will enter the grocery name and grocery cost it will be added into the groceries table
+  } else {
+    res.render('newGrocery', {title: "Add Grocery", msg: "ERROR: Please refill form."})
+  }
+})
 
 
 module.exports = router;
